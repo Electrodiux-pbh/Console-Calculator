1235be1648c3d026ecb18b762eadd5e1dc37cdac
Electrodiux_pbh <86020138+Electrodiux-pbh@users.noreply.github.com>
2024-05-08 20:13:06 +0100

    Negative number processing

--- main.cpp
+++ main.cpp
@@ -15,16 +15,16 @@ enum class Operation {
 
 Operation getOperationFromChar(char c) {
 	switch (c) {
-		case '+':
-			return Operation::SUM;
-		case '-':
-			return Operation::MIN;
-		case '*':
-			return Operation::MUL;
-		case '/':
-			return Operation::DIV;
-		case '^':
-			return Operation::POW;
+	case '+':
+		return Operation::SUM;
+	case '-':
+		return Operation::MIN;
+	case '*':
+		return Operation::MUL;
+	case '/':
+		return Operation::DIV;
+	case '^':
+		return Operation::POW;
 	}
 	return Operation::NONE;
 }
@@ -34,70 +34,70 @@ constexpr int MAX_PRIORITY = 3;
 
 int getPriority(Operation operation) {
 	switch (operation) {
-		case Operation::SUM:
-		case Operation::MIN:
-			return 1;
-		case Operation::MUL:
-		case Operation::DIV:
-			return 2;
-		case Operation::POW:
-			return 3;
-		default:
-			return 0;
+	case Operation::SUM:
+	case Operation::MIN:
+		return 1;
+	case Operation::MUL:
+	case Operation::DIV:
+		return 2;
+	case Operation::POW:
+		return 3;
+	default:
+		return 0;
 	}
 }
 
 // Tokens
 
 struct Token {
-    virtual double resolve() = 0; // Pure virtual function
-    virtual ~Token() {} // Virtual destructor
+	virtual double resolve() = 0; // Pure virtual function
+	virtual ~Token() {} // Virtual destructor
 };
 
 struct NumberToken : Token {
-	
+
 	double value;
-	
+
 	NumberToken(double value) {
 		this->value = value;
 	}
-	
+
 	double resolve() override {
 		return value;
 	}
-	
+
 };
 
 struct OperationToken : Token {
-    Operation operation;
+	Operation operation;
 
-    std::unique_ptr<Token> a, b;
+	std::unique_ptr<Token> a, b;
 
-    OperationToken(Operation operation, std::unique_ptr<Token> a, std::unique_ptr<Token> b)
-        : operation(operation), a(std::move(a)), b(std::move(b)) {}
+	OperationToken(Operation operation, std::unique_ptr<Token> a, std::unique_ptr<Token> b)
+		: operation(operation), a(std::move(a)), b(std::move(b)) {}
 
-    OperationToken(Operation operation, std::unique_ptr<Token> a)
-        : operation(operation), a(std::move(a)), b(std::make_unique<NumberToken>(0)) {}
+	OperationToken(Operation operation, std::unique_ptr<Token> a)
+		: operation(operation), a(std::move(a)), b(std::make_unique<NumberToken>(0)) {}
 
-    double resolve() override {
-        double aValue = a->resolve();
-        double bValue = b->resolve();
+	double resolve() override {
+		double aValue = a->resolve();
+		double bValue = b->resolve();
 
-        switch(operation) {
-            case Operation::SUM:
-                return aValue + bValue;
-            case Operation::MIN:
-                return aValue - bValue;
-            case Operation::MUL:
-                return aValue * bValue;
-            case Operation::DIV:
-                return aValue / bValue;
-			case Operation::POW:
-				return std::pow(aValue, bValue);
-            default:
-                return 0;
-        }
-    }
+		switch (operation) {
+		case Operation::SUM:
+			return aValue + bValue;
+		case Operation::MIN:
+			return aValue - bValue;
+		case Operation::MUL:
+			return aValue * bValue;
+		case Operation::DIV:
+			return aValue / bValue;
+		case Operation::POW:
+			return std::pow(aValue, bValue);
+		default:
+			return 0;
+		}
+	}
 };
 
 // Parser
@@ -138,16 +138,16 @@ std::unique_ptr<Token> parseToken(std::string expression) {
 				}
 
 				if (open == close) {
-					std::string subExpression = expression.substr(i + 1, j - i - 1); // Get the substring between the parenthesis
-					std::unique_ptr<Token> token = parseToken(subExpression); // Parse the substring
+					std::string subExpression = expression.substr(i + 1, j - i - 1);
+					std::unique_ptr<Token> token = parseToken(subExpression);
 
-					std::string newExpression = expression.substr(0, i) + std::to_string(token->resolve()) + expression.substr(j + 1); // Replace the substring with the result of the token
+					std::string newExpression = expression.substr(0, i) + std::to_string(token->resolve()) + expression.substr(j + 1);
 					return parseToken(newExpression);
 				}
 			}
 		}
 	}
-	
+
 	// Iterate over the priority of the operations
 	// We will first resolve the operations with priority 1 and then the operations with priority 2
 	// Because creating the tree the operations that are more deep (proirity 2) will be resolved first
@@ -171,6 +171,10 @@ std::unique_ptr<Token> parseToken(std::string expression) {
 			// Get right and left side of the operation
 			right = expression.substr(i + 1);
 			left = expression.substr(0, i);
+
+			if (left == "" && operation == Operation::MIN) { // If there is a minus sign and there is no left member ignore so it process as negative number
+				continue;
+			}
 
 			// Parse the right and left side of the operation
 			std::unique_ptr<Token> a = parseToken(left);
@@ -199,12 +203,12 @@ void unrecognizedAction() {
 
 int main() {
 	std::cout << "Welcome to calculator, type an acction to do (type h for help)" << std::endl;
-	
-	while(true) { // Keep the program alive indefenetly
+
+	while (true) { // Keep the program alive indefenetly
 		std::string action;
-		
+
 		std::getline(std::cin, action);
-		
+
 		if (action == "h") {
 			help();
 			continue;
@@ -214,10 +218,10 @@ int main() {
 		}
 		else if (action == "o") {
 			std::cout << "Enter a operation: ";
-			
+
 			std::string expression;
 			std::getline(std::cin, expression);
-			
+
 			std::cout << std::endl;
 
 			// Remove all spaces from the expression
@@ -228,12 +232,12 @@ int main() {
 			std::unique_ptr<Token> token = parseToken(expressionWithoutSpaces);
 			double result = token->resolve();
 
-			std::cout << "The result from (" << expression << ") is " << result << std::endl;		
+			std::cout << "The result from (" << expression << ") is " << result << std::endl;
 		}
 		else {
 			unrecognizedAction();
 		}
 	}
-	
+
 	return 0;
 }
